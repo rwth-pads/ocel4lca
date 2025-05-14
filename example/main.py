77fab4b4b0b846c4143431ab814243572bed8583
Nina Graves <graves@pads.rwth-aachen.de>
2025-05-14 08:57:51 +0200

    :sparkles: added all files for the example and the OCEAn repository to this single repository associated with the paper

---
+++ example/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ocpm4lca!")
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from ocpm4lca!")
-
-
-if __name__ == "__main__":
-    main()
